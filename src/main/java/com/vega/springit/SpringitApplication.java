7d8ee37b6f0abdf9ee160dab032b9b95cf403d93
Rino <rlupetin@pm.me>
2020-12-03 13:37:50 -0500

    Adding a welcome message.

--- src/main/java/com/vega/springit/SpringitApplication.java
+++ src/main/java/com/vega/springit/SpringitApplication.java
@@ -8,6 +8,7 @@ public class SpringitApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringitApplication.class, args);
+        System.out.println("Welcome to SpringIt Motherfucker!!!");
     }
 
 }
