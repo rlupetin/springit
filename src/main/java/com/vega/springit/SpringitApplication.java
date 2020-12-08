181a1ce5a37d2ae932d04453c60593e1821690bf
Rino <rlupetin@pm.me>
2020-12-08 11:01:41 -0500

    Adding a welcome message.

--- src/main/java/com/vega/springit/SpringitApplication.java
+++ src/main/java/com/vega/springit/SpringitApplication.java
@@ -1,12 +1,17 @@
 package com.vega.springit;
 
 import com.vega.springit.config.SpringItProperties;
+import org.springframework.beans.factory.ListableBeanFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 @EnableConfigurationProperties(SpringItProperties.class)
@@ -14,16 +19,29 @@ public class SpringitApplication {
 
     @Autowired
     private SpringItProperties springItProperties;
+    @Autowired
+    private ApplicationContext applicationContext;
+
     public static void main(String[] args) {
         SpringApplication.run(SpringitApplication.class, args);
         System.out.println("Welcome to SpringIt Motherfucker!!!");
+
     }
 
     @Bean
     CommandLineRunner runner(){
+
         return args -> {
             System.out.println("Welcome Message is "+springItProperties.getWelcomeMsg());
-            System.out.println("Current port is "+ springItProperties.getPortNumber());
+            System.out.println("This is something we would only do in dev");
+
+            String[] beans = applicationContext.getBeanDefinitionNames();
+         /*   Arrays.sort(beans);
+
+            for (String bean: beans)
+                System.out.println(bean);*/
+
+
         };
     }
 
--- src/main/java/com/vega/springit/config/SpringItProperties.java
+++ src/main/java/com/vega/springit/config/SpringItProperties.java
@@ -5,15 +5,6 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties("springit")
 public class SpringItProperties {
     private String welcomeMsg = "Hey there SpringIt";
-    private Integer portNumber = 8080;
-
-    public Integer getPortNumber() {
-        return portNumber;
-    }
-
-    public void setPortNumber(Integer portNumber) {
-        this.portNumber = portNumber;
-    }
 
     public String getWelcomeMsg() {
         return welcomeMsg;
